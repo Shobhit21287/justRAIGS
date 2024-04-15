7071c6eb7c8b831ae328e37a137eb39d2b642064
Shobhit21287 <Shobhit21287@iiitd.ac.in>
2024-04-15 22:19:52 +0530

    iterate through tiff

--- helper.py
+++ helper.py
@@ -6,6 +6,68 @@ import os
 import SimpleITK as sitk
 from PIL import Image
 import tifffile
+import random
+import torch
+from torchvision import transforms
+import torchvision
+from torchvision import models
+import numpy
+from PIL import Image
+import torch.nn as nn
+
+test_transform=transforms.Compose([
+            transforms.Resize((64, 64)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+model=models.densenet201(pretrained=False)
+for parameter in model.parameters():
+    parameter.requires_grad=False
+model.classifier = nn.Identity()
+custom_layer = nn.Sequential(
+    nn.Linear(1920,1920),
+    nn.Dropout(0.25),
+    nn.ReLU(),
+    nn.Linear(1920,512),
+    nn.Dropout(0.25),
+    nn.ReLU(),
+    nn.Linear(512,256),
+    nn.Dropout(0.25),
+    nn.ReLU(),
+    nn.Linear(256,256),
+    nn.ReLU(),
+    nn.Linear(256,128),
+    nn.ReLU(),
+    nn.Linear(128,2)
+)
+model = nn.Sequential(model, custom_layer)
+# pth = "/Users/chaitanyadua/Downloads/justRAIGS-main/model.pth"
+pth = "/opt/algorithm/model.pth"
+model.load_state_dict(torch.load(pth,map_location=torch.device('cpu')))
+#model.load_state_dict(torch.load("/Users/chaitanyadua/Downloads/justRAIGS-main/model.pth",map_location=torch.device('cpu'))) 
+model.eval()
+
+model2=models.resnet18(pretrained=False)
+for parameter in model2.parameters():
+    parameter.requires_grad=True
+features_last_fc = model2.fc.in_features
+model2.fc=torch.nn.Sequential(
+    nn.Linear(features_last_fc,features_last_fc),
+    nn.ReLU(),
+    nn.Dropout(0.25),
+    nn.Linear(features_last_fc,features_last_fc),
+    nn.ReLU(),
+    nn.Dropout(0.5),
+    nn.Linear(features_last_fc,256),
+    nn.ReLU(),
+    nn.Linear(256,128),
+    nn.ReLU(),
+    nn.Linear(128,10),
+)
+#model2.load_state_dict(torch.load("C:\\Users\\Shobhit\\Desktop\\IIITacad\\Sem6\\ML_project\\sample\\model_one_hot.pth")) 
+#model2.load_state_dict(torch.load("/Users/chaitanyadua/Downloads/justRAIGS-main/model_one_hot.pth",map_location=torch.device('cpu'))) 
+model2.load_state_dict(torch.load("/opt/algorithm/model_one_hot.pth",map_location=torch.device('cpu')))
+model2.eval()
 
 DEFAULT_GLAUCOMATOUS_FEATURES = {
     "appearance neuroretinal rim superiorly": None,
@@ -19,18 +81,16 @@ DEFAULT_GLAUCOMATOUS_FEATURES = {
     "laminar dots": None,
     "large cup": None,
 }
-
+is_referable_glaucoma_stacked = []
+is_referable_glaucoma_likelihood_stacked = []
+glaucomatous_features_stacked = []
 def inference_tasks():
     #test = "C:/Users/Shobhit/Desktop/IIITacad/sem6/ML_project/sample/Example algorithm/test"
-    # test = "/Users/chaitanyadua/Downloads/justRAIGS-main/Example algorithm/test"
-    # input_files = [x for x in Path(test + "/input").rglob("*") if x.is_file()]
+    #test = "/Users/chaitanyadua/Downloads/justRAIGS-main/Example algorithm/test"
+    #input_files = [x for x in Path(test + "/input").rglob("*") if x.is_file()]
     input_files = [x for x in Path("/input").rglob("*") if x.is_file()]
-
     print("Input Files:")
     pprint(input_files)
-    is_referable_glaucoma_stacked = []
-    is_referable_glaucoma_likelihood_stacked = []
-    glaucomatous_features_stacked = []
     def save_prediction(
             is_referable_glaucoma,
             likelihood_referable_glaucoma,
@@ -45,18 +105,22 @@ def inference_tasks():
 
     for file_path in input_files:
         if file_path.suffix == ".mha":  # A single image
-            yield from single_file_inference(image_file=file_path, callback=save_prediction)
+            single_file_inference(image_file=file_path)
+            write_referable_glaucoma_decision(is_referable_glaucoma_stacked)
+            write_referable_glaucoma_decision_likelihood(
+            is_referable_glaucoma_likelihood_stacked
+            )
+            write_glaucomatous_features(glaucomatous_features_stacked)
         elif file_path.suffix == ".tif" or file_path.suffix == ".tiff":  # A stack of images
-            yield from stack_inference(stack=file_path, callback=save_prediction)
+            stack_inference(stack=file_path)
+            write_referable_glaucoma_decision(is_referable_glaucoma_stacked)
+            write_referable_glaucoma_decision_likelihood(
+            is_referable_glaucoma_likelihood_stacked
+            )
+            write_glaucomatous_features(glaucomatous_features_stacked)
 
-    write_referable_glaucoma_decision(is_referable_glaucoma_stacked)
-    write_referable_glaucoma_decision_likelihood(
-        is_referable_glaucoma_likelihood_stacked
-    )
-    write_glaucomatous_features(glaucomatous_features_stacked)
 
-
-def single_file_inference(image_file, callback):
+def single_file_inference(image_file):
     with tempfile.TemporaryDirectory() as temp_dir:
         image = sitk.ReadImage(image_file)
         if image.GetDimension()==3:
@@ -73,28 +137,48 @@ def single_file_inference(image_file, callback):
 
             # Save the 2D slice as a JPG file
             sitk.WriteImage(image, str(output_path))
+        jpg_image_file_name=output_path
+        image = Image.open(jpg_image_file_name)
+        image_array=numpy.array(image)
+        if(image_array.ndim==2):
+            # pil_image = Image.fromarray(image_array)
+            # image=test_transform_2(pil_image)
+            rgb_image = numpy.zeros((3,image_array.shape[0], image_array.shape[1]))
+            rgb_image[0, :, :] = image_array
+            rgb_image[1, :, :] = image_array
+            rgb_image[2, :, :] = image_array
+            pil_image = Image.fromarray(rgb_image,mode='RGB')
+            image = test_transform(pil_image)
+        else:
+            image = test_transform(image)
+        image = image.unsqueeze(0)
+        # print(image.shape)
+        # image = image.unsqueeze(0)
+        is_referable_glaucoma_likelihood = model(image)
+        is_referable_glaucoma_likelihood = torch.sigmoid(is_referable_glaucoma_likelihood).detach().numpy()
+        is_referable_glaucoma = bool(is_referable_glaucoma_likelihood[0][0] < is_referable_glaucoma_likelihood[0][1])
+        is_referable_glaucoma_likelihood = float(is_referable_glaucoma_likelihood[0][1])
+        features2 = {}
+        features = model2(image)
+        probabilities=torch.sigmoid(features)
+        predicted_labels = torch.round(probabilities)
+        features = predicted_labels.detach().numpy()[0].astype(int).astype(bool)
+        #print(probabilities,features)
+        ct = 0
+        for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
+            features2[i] = features[ct].item()
+            ct += 1
+        is_referable_glaucoma_stacked.append(is_referable_glaucoma)
+        is_referable_glaucoma_likelihood_stacked.append(is_referable_glaucoma_likelihood)
+        if features2 is not None:
+            glaucomatous_features_stacked.append({**DEFAULT_GLAUCOMATOUS_FEATURES, **features2})
+        else:
+            glaucomatous_features_stacked.append(DEFAULT_GLAUCOMATOUS_FEATURES)
+        # yield output_path,callback
         del image
-        # Call back that saves the result
-        def save_prediction(
-            is_referable_glaucoma,
-            likelihood_referable_glaucoma,
-            glaucomatous_features=None,
-        ):
-            glaucomatous_features = (
-                glaucomatous_features or DEFAULT_GLAUCOMATOUS_FEATURES
-            )
-            write_referable_glaucoma_decision([is_referable_glaucoma])
-            write_referable_glaucoma_decision_likelihood(
-                [likelihood_referable_glaucoma]
-            )
-            write_glaucomatous_features(
-                [{**DEFAULT_GLAUCOMATOUS_FEATURES, **glaucomatous_features}]
-            )
-
-        yield output_path, callback
 
 
-def stack_inference(stack, callback):
+def stack_inference(stack):
     # de_stacked_images = []
     # Unpack the stack
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -111,9 +195,9 @@ def stack_inference(stack, callback):
                 # tiff_image.seek(page_num)
                 tiff_page=tiff_image.pages[page_num]
                 image=Image.fromarray(tiff_page.asarray())
-                if max(image.size) >64:
+                if max(image.size) >200:
                 # Resize the image to reduce its dimensions
-                    image = image.resize((64, 64))
+                    image = image.resize((200, 200))
                 # Define the output file path
                 print(f"image_{page_num+1}")
                 output_path = Path(temp_dir) / f"image_{page_num + 1}.jpg"
@@ -122,30 +206,65 @@ def stack_inference(stack, callback):
                 # de_stacked_images.append(output_path)
 
                 print(f"De-Stacked {output_path}")
-                yield output_path,callback
+                jpg_image_file_name=output_path
+                image = Image.open(jpg_image_file_name)
+                image_array=numpy.array(image)
+                if(image_array.ndim==2):
+                    # pil_image = Image.fromarray(image_array)
+                    # image=test_transform_2(pil_image)
+                    rgb_image = numpy.zeros((3,image_array.shape[0], image_array.shape[1]))
+                    rgb_image[0, :, :] = image_array
+                    rgb_image[1, :, :] = image_array
+                    rgb_image[2, :, :] = image_array
+                    pil_image = Image.fromarray(rgb_image,mode='RGB')
+                    image = test_transform(pil_image)
+                else:
+                    image = test_transform(image)
+                image = image.unsqueeze(0)
+                # print(image.shape)
+                # image = image.unsqueeze(0)
+                is_referable_glaucoma_likelihood = model(image)
+                is_referable_glaucoma_likelihood = torch.sigmoid(is_referable_glaucoma_likelihood).detach().numpy()
+                is_referable_glaucoma = bool(is_referable_glaucoma_likelihood[0][0] < is_referable_glaucoma_likelihood[0][1])
+                is_referable_glaucoma_likelihood = float(is_referable_glaucoma_likelihood[0][1])
+                features2 = {}
+                features = model2(image)
+                probabilities=torch.sigmoid(features)
+                predicted_labels = torch.round(probabilities)
+                features = predicted_labels.detach().numpy()[0].astype(int).astype(bool)
+                #print(probabilities,features)
+                ct = 0
+                for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
+                    features2[i] = features[ct].item()
+                    ct += 1
 
-        # Loop over the images, and generate the actual tasks
-        # for index, image in enumerate(de_stacked_images):
-            # Call back that saves the result
-            # yield image, callback
+                is_referable_glaucoma_stacked.append(is_referable_glaucoma)
+                is_referable_glaucoma_likelihood_stacked.append(is_referable_glaucoma_likelihood)
+                if features2 is not None:
+                    glaucomatous_features_stacked.append({**DEFAULT_GLAUCOMATOUS_FEATURES, **features2})
+                else:
+                    glaucomatous_features_stacked.append(DEFAULT_GLAUCOMATOUS_FEATURES)
+                # yield output_path,callback
+
 
 # current_dir = os.getcwd()
 
 #test = "C:/Users/Shobhit/Desktop/IIITacad/sem6/ML_project/sample/Example algorithm/test"
-# test = "/Users/chaitanyadua/Downloads/justRAIGS-main/Example algorithm/test"
+#test = "/Users/chaitanyadua/Downloads/justRAIGS-main/Example algorithm/test"
 def write_referable_glaucoma_decision(result):
-    # with open(test + "/output/multiple-referable-glaucoma-binary.json", "w") as f:
+    #with open(test + "/output/multiple-referable-glaucoma-binary.json", "w+") as f:
     with open("/output/multiple-referable-glaucoma-binary.json", "w") as f:
         f.write(json.dumps(result))
+    # write_json(test+"/output/multiple-referable-glaucoma-binary.json",result)
 
 
 def write_referable_glaucoma_decision_likelihood(result):
-    # with open(test + "/output/multiple-referable-glaucoma-likelihoods.json", "w") as f:
+    #with open(test + "/output/multiple-referable-glaucoma-likelihoods.json", "w+") as f:
     with open("/output/multiple-referable-glaucoma-likelihoods.json", "w") as f:
         f.write(json.dumps(result))
 
 
 def write_glaucomatous_features(result):
-    # with open(test + "/output/stacked-referable-glaucomatous-features.json", "w") as f:
+    #with open(test + "/output/stacked-referable-glaucomatous-features.json", "w+") as f:
     with open("/output/stacked-referable-glaucomatous-features.json", "w") as f:
         f.write(json.dumps(result))
--- inference.py
+++ inference.py
@@ -12,136 +12,139 @@ import torch.nn as nn
 def run():
     _show_torch_cuda_info()
 
-    test_transform=transforms.Compose([
-            transforms.Resize((64, 64)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        ])
-    # test_transform_2=transforms.Compose([
-            # transforms.Resize((64, 64)),
-            # transforms.ToTensor(),
-            # transforms.Normalize(mean=0.485, std=0.229)
-        # ])
+#     test_transform=transforms.Compose([
+#             transforms.Resize((64, 64)),
+#             transforms.ToTensor(),
+#             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+#         ])
+#     # test_transform_2=transforms.Compose([
+#             # transforms.Resize((64, 64)),
+#             # transforms.ToTensor(),
+#             # transforms.Normalize(mean=0.485, std=0.229)
+#         # ])
 
 
-#model=models.resnet18(pretrained=True)
-    model=models.densenet201(pretrained=False)
-    for parameter in model.parameters():
-        parameter.requires_grad=False
-    model.classifier = nn.Identity()
-    custom_layer = nn.Sequential(
-        nn.Linear(1920,1920),
-        nn.Dropout(0.25),
-        nn.ReLU(),
-        nn.Linear(1920,512),
-        nn.Dropout(0.25),
-        nn.ReLU(),
-        nn.Linear(512,256),
-        nn.Dropout(0.25),
-        nn.ReLU(),
-        nn.Linear(256,256),
-        nn.ReLU(),
-        nn.Linear(256,128),
-        nn.ReLU(),
-        nn.Linear(128,2)
-    )
-    model = nn.Sequential(model, custom_layer)
-    # pth = "/Users/chaitanyadua/Downloads/justRAIGS-main/model.pth"
-    pth = "/opt/algorithm/model.pth"
-    model.load_state_dict(torch.load(pth,map_location=torch.device('cpu')))
-    # model.load_state_dict(torch.load("/Users/chaitanyadua/Downloads/justRAIGS-main/model.pth",map_location=torch.device('cpu'))) 
-    model.eval()
+# #model=models.resnet18(pretrained=True)
+#     model=models.densenet201(pretrained=False)
+#     for parameter in model.parameters():
+#         parameter.requires_grad=False
+#     model.classifier = nn.Identity()
+#     custom_layer = nn.Sequential(
+#         nn.Linear(1920,1920),
+#         nn.Dropout(0.25),
+#         nn.ReLU(),
+#         nn.Linear(1920,512),
+#         nn.Dropout(0.25),
+#         nn.ReLU(),
+#         nn.Linear(512,256),
+#         nn.Dropout(0.25),
+#         nn.ReLU(),
+#         nn.Linear(256,256),
+#         nn.ReLU(),
+#         nn.Linear(256,128),
+#         nn.ReLU(),
+#         nn.Linear(128,2)
+#     )
+#     model = nn.Sequential(model, custom_layer)
+#     # pth = "/Users/chaitanyadua/Downloads/justRAIGS-main/model.pth"
+#     # pth = "/opt/algorithm/model.pth"
+#     # model.load_state_dict(torch.load(pth,map_location=torch.device('cpu')))
+#     model.load_state_dict(torch.load("/Users/chaitanyadua/Downloads/justRAIGS-main/model.pth",map_location=torch.device('cpu'))) 
+#     model.eval()
 
-    model2=models.resnet18(pretrained=False)
-    for parameter in model2.parameters():
-        parameter.requires_grad=True
-    features_last_fc = model2.fc.in_features
-    model2.fc=torch.nn.Sequential(
-        nn.Linear(features_last_fc,features_last_fc),
-        nn.ReLU(),
-        nn.Dropout(0.25),
-        nn.Linear(features_last_fc,features_last_fc),
-        nn.ReLU(),
-        nn.Dropout(0.5),
-        nn.Linear(features_last_fc,256),
-        nn.ReLU(),
-        nn.Linear(256,128),
-        nn.ReLU(),
-        nn.Linear(128,10),
-    )
-    #model2.load_state_dict(torch.load("C:\\Users\\Shobhit\\Desktop\\IIITacad\\Sem6\\ML_project\\sample\\model_one_hot.pth")) 
-    # model2.load_state_dict(torch.load("/Users/chaitanyadua/Downloads/justRAIGS-main/model_one_hot.pth",map_location=torch.device('cpu'))) 
-    model2.load_state_dict(torch.load("/opt/algorithm/model_one_hot.pth",map_location=torch.device('cpu')))
-    model2.eval()
+#     model2=models.resnet18(pretrained=False)
+#     for parameter in model2.parameters():
+#         parameter.requires_grad=True
+#     features_last_fc = model2.fc.in_features
+#     model2.fc=torch.nn.Sequential(
+#         nn.Linear(features_last_fc,features_last_fc),
+#         nn.ReLU(),
+#         nn.Dropout(0.25),
+#         nn.Linear(features_last_fc,features_last_fc),
+#         nn.ReLU(),
+#         nn.Dropout(0.5),
+#         nn.Linear(features_last_fc,256),
+#         nn.ReLU(),
+#         nn.Linear(256,128),
+#         nn.ReLU(),
+#         nn.Linear(128,10),
+#     )
+#     #model2.load_state_dict(torch.load("C:\\Users\\Shobhit\\Desktop\\IIITacad\\Sem6\\ML_project\\sample\\model_one_hot.pth")) 
+#     model2.load_state_dict(torch.load("/Users/chaitanyadua/Downloads/justRAIGS-main/model_one_hot.pth",map_location=torch.device('cpu'))) 
+#     # model2.load_state_dict(torch.load("/opt/algorithm/model_one_hot.pth",map_location=torch.device('cpu')))
+#     model2.eval()
     
 
     # model_one_hot_encode = torch.load("model_multilabel.pth")
     # model_one_hot_encode.eval()
-    
-    for jpg_image_file_name, save_prediction in inference_tasks():
-        # Do inference, possibly something better performant
-        print(f"Running inference on {jpg_image_file_name}")
-        # For example: use Pillow to read the jpg file and convert it to a NumPY array:
-        image = Image.open(jpg_image_file_name)
-        image_array=numpy.array(image)
-        if(image_array.ndim==2):
-            # pil_image = Image.fromarray(image_array)
-            # image=test_transform_2(pil_image)
-            rgb_image = numpy.zeros((3,image_array.shape[0], image_array.shape[1]))
-            rgb_image[0, :, :] = image_array
-            rgb_image[1, :, :] = image_array
-            rgb_image[2, :, :] = image_array
-            pil_image = Image.fromarray(rgb_image,mode='RGB')
-            image = test_transform(pil_image)
-        else:
-            image = test_transform(image)
-        image = image.unsqueeze(0)
-        # print(image.shape)
-        # image = image.unsqueeze(0)
-        is_referable_glaucoma_likelihood = model(image)
-        is_referable_glaucoma_likelihood = torch.sigmoid(is_referable_glaucoma_likelihood).detach().numpy()
-        is_referable_glaucoma = bool(is_referable_glaucoma_likelihood[0][0] < is_referable_glaucoma_likelihood[0][1])
-        is_referable_glaucoma_likelihood = float(is_referable_glaucoma_likelihood[0][1])
-        features2 = {}
-        features = model2(image)
-        probabilities=torch.sigmoid(features)
-        predicted_labels = torch.round(probabilities)
-        features = predicted_labels.detach().numpy()[0].astype(int).astype(bool)
-        #print(probabilities,features)
-        ct = 0
-        for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
-            features2[i] = features[ct].item()
-            ct += 1
-        # if is_referable_glaucoma:
-        #     # features = {
-        #     #     k: random.choice([True, False])
-        #     #     for k, v in DEFAULT_GLAUCOMATOUS_FEATURES.items()
-        #     # }
-        # else:
-        #     features2 = None
-        # #     ct = 0
-        # #     for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
-        # #         features2[i] = False
-        # #         ct += 1
+    #print("HI")
+    inference_tasks()
+    print(DEFAULT_GLAUCOMATOUS_FEATURES)
+    # print(list(inference_tasks()))
+    # for jpg_image_file_name, save_prediction in inference_tasks():
+    #     # Do inference, possibly something better performant
+    #     print(f"Running inference on {jpg_image_file_name}")
+    #     # For example: use Pillow to read the jpg file and convert it to a NumPY array:
+    #     image = Image.open(jpg_image_file_name)
+    #     image_array=numpy.array(image)
+    #     if(image_array.ndim==2):
+    #         # pil_image = Image.fromarray(image_array)
+    #         # image=test_transform_2(pil_image)
+    #         rgb_image = numpy.zeros((3,image_array.shape[0], image_array.shape[1]))
+    #         rgb_image[0, :, :] = image_array
+    #         rgb_image[1, :, :] = image_array
+    #         rgb_image[2, :, :] = image_array
+    #         pil_image = Image.fromarray(rgb_image,mode='RGB')
+    #         image = test_transform(pil_image)
+    #     else:
+    #         image = test_transform(image)
+    #     image = image.unsqueeze(0)
+    #     # print(image.shape)
+    #     # image = image.unsqueeze(0)
+    #     is_referable_glaucoma_likelihood = model(image)
+    #     is_referable_glaucoma_likelihood = torch.sigmoid(is_referable_glaucoma_likelihood).detach().numpy()
+    #     is_referable_glaucoma = bool(is_referable_glaucoma_likelihood[0][0] < is_referable_glaucoma_likelihood[0][1])
+    #     is_referable_glaucoma_likelihood = float(is_referable_glaucoma_likelihood[0][1])
+    #     features2 = {}
+    #     features = model2(image)
+    #     probabilities=torch.sigmoid(features)
+    #     predicted_labels = torch.round(probabilities)
+    #     features = predicted_labels.detach().numpy()[0].astype(int).astype(bool)
+    #     #print(probabilities,features)
+    #     ct = 0
+    #     for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
+    #         features2[i] = features[ct].item()
+    #         ct += 1
+    #     # if is_referable_glaucoma:
+    #     #     # features = {
+    #     #     #     k: random.choice([True, False])
+    #     #     #     for k, v in DEFAULT_GLAUCOMATOUS_FEATURES.items()
+    #     #     # }
+    #     # else:
+    #     #     features2 = None
+    #     # #     ct = 0
+    #     # #     for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
+    #     # #         features2[i] = False
+    #     # #         ct += 1
         
-        # for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
-        #     features2[i] = False
-        # features = model2(image)
-        # probabilities=torch.sigmoid(features)
-        # predicted_labels = torch.round(probabilities)
-        # features = predicted_labels.detach().numpy()[0].astype(int).astype(bool)
-        # ct = 0
-        # for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
-        #     features2[i] = features[ct].item()
-        #     ct += 1  
+    #     # for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
+    #     #     features2[i] = False
+    #     # features = model2(image)
+    #     # probabilities=torch.sigmoid(features)
+    #     # predicted_labels = torch.round(probabilities)
+    #     # features = predicted_labels.detach().numpy()[0].astype(int).astype(bool)
+    #     # ct = 0
+    #     # for i in DEFAULT_GLAUCOMATOUS_FEATURES.keys():
+    #     #     features2[i] = features[ct].item()
+    #     #     ct += 1  
 
-        # Finally, save the answer
-        save_prediction(
-            is_referable_glaucoma,
-            is_referable_glaucoma_likelihood,
-            features2,
-        )
-    return 0
+    #     # Finally, save the answer
+    #     save_prediction(
+    #         is_referable_glaucoma,
+    #         is_referable_glaucoma_likelihood,
+    #         features2,
+    #     )
+    # return 0
 
 
 def _show_torch_cuda_info():
